9b248425292ed72399d1d1997d4552f3f71671c6
Blamp26 <98893587+Blamp26@users.noreply.github.com>
2024-10-31 19:44:52 +0200

    esp32_reciver.ino

--- esp32_reciver.ino
+++ esp32_reciver.ino
@@ -1,23 +1,22 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+#include <VirtualWire.h>  // Ensure you have the VirtualWire library
 
-RF24 radio(4, 5);  // CE, CSN
-
-const byte address[6] = "00001";
-char receivedData[32];
+const int RX_PIN = 22;  // Pin connected to RX470C-V01 data pin
 
 void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_LOW);
-  radio.startListening();
+    Serial.begin(9600);
+    vw_set_rx_pin(RX_PIN);
+    vw_setup(2000);  // Bits per second
+    vw_rx_start();   // Start the receiver
 }
 
 void loop() {
-  if (radio.available()) {
-    radio.read(&receivedData, sizeof(receivedData));
-    Serial.println(receivedData);
-  }
+    uint8_t buf[16];  // Buffer to hold the incoming message
+    uint8_t buflen = sizeof(buf);
+
+    if (vw_get_message(buf, &buflen)) {
+        // Message received
+        buf[buflen] = '\0';  // Null-terminate the string
+        Serial.print("Received: ");
+        Serial.println((char *)buf);
+    }
 }
